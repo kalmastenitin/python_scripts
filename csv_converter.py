fadacef9b7d542869b4e515349a7f1a4757d0e73
kalmastenitin <nknckalmaste@gmail.com>
2019-12-06 20:50:53 +0530

    added comments in csv_converter file

--- csv_converter.py
+++ csv_converter.py
@@ -2,6 +2,12 @@ import sqlite3
 import pandas as pd
 from sqlalchemy import create_engine
 
-engine = create_engine('sqlite:///new_data.db')
+'''This script will used to insert all sql data into database
+   Doing Operations on database is faster than using raw excel/csv file
+   If you have more RAM in your machine then you can use this script to load your database into RAM
+'''
+
+engine = create_engine('sqlite:///new_data.db') #Create New Database File
+#We can use 'sqlite://' to use database inside your RAM so large data will parse easily
 df = pd.read_csv('bitcoin_data.csv')
-df.to_sql('bitcoin_data', engine, if_exists='replace', index=False)
+df.to_sql('bitcoin_data', engine, if_exists='replace', index=False) #Create Table with name=bitcoin_data
