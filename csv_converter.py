f72d64da8150ea81a49bb41cff5b62b9e68f13f0
kalmastenitin <nknckalmaste@gmail.com>
2019-12-06 21:04:19 +0530

    Added Sql commands to test

--- csv_converter.py
+++ csv_converter.py
@@ -11,3 +11,15 @@ engine = create_engine('sqlite:///new_data.db') #Create New Database File
 #We can use 'sqlite://' to use database inside your RAM so large data will parse easily
 df = pd.read_csv('bitcoin_data.csv')
 df.to_sql('bitcoin_data', engine, if_exists='replace', index=False) #Create Table with name=bitcoin_data
+
+results = engine.execute('SELECT * FROM bitcoin_data ORDER BY High DESC LIMIT 1;')
+MAX_result = pd.DataFrame(results, columns=df.columns)
+print('MAX_HIGH \n',MAX_result)
+
+results = engine.execute('SELECT * FROM bitcoin_data ORDER BY High ASC LIMIT 1;')
+MIN_result = pd.DataFrame(results, columns=df.columns)
+print('MIN_HIGH: \n',MAX_result)
+
+results = engine.execute('SELECT SUM(Volume) FROM bitcoin_data;')
+total_volume = pd.DataFrame(results)
+print('TOTAL VOLUME TRADED:',total_volume)
